61c3475ac00a597294e4558019b3b401d745a7cf
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-07 19:24:17 -0500

    inicio

---
+++ src/main/java/com/cibertec/SwJava17Semana04RestMysqlSqlApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwJava17Semana04RestMysqlSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwJava17Semana04RestMysqlSqlApplication.class, args);
+	}
+
+}
